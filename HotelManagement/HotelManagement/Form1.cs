7cb495a3ee265e9af28558d25aa812ea48581f4c
Jau2 <124139712+jpcorsta@users.noreply.github.com>
2025-04-25 20:16:03 +0200

    Add files via upload

---
+++ HotelManagement/HotelManagement/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelManagement
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnLogin_Click(object sender, EventArgs e)
+		{					                        
+				SqlConnection con = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=HotelDB;Integrated Security=True;TrustServerCertificate=True");
+				con.Open();
+				string username = txtUsername.Text;
+				string password = txtPassword.Text;
+			    SqlCommand cmd = new SqlCommand("select UserName,Password from logintab where UserName='" + txtUsername.Text + "'and Password='" + txtPassword.Text + "'", con);
+			    SqlDataAdapter da = new SqlDataAdapter(cmd);
+			    DataTable dt = new DataTable();
+			    da.Fill(dt);
+			    if (dt.Rows.Count > 0 )
+				{ 
+					Main mn = new Main();
+				    mn.Show();
+				}
+				else
+				{ 
+					MessageBox.Show("Invalid Login please check username and password");
+				}
+				con.Close();
+		}
+	}
+}
